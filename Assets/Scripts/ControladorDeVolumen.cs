76ac8e56b7ea2de6688b4da2d8208f1b42b10024
Luis Plata <www.luisplata@gmail.com>
2020-06-29 11:20:34 -0500

    Se agrega el sonido al proyecto, puedo decir que esta completo

---
+++ Assets/Scripts/ControladorDeVolumen.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControladorDeVolumen : MonoBehaviour
+{
+    public AudioSource audio;
+    public Slider slider;
+
+    private void Start()
+    {
+        if (PlayerPrefs.HasKey("volumenGeneral"))
+        {
+            slider.value = PlayerPrefs.GetFloat("volumenGeneral");
+        }
+        else
+        {
+            slider.value = 1;
+        }
+        audio.volume = slider.value;
+    }
+    //por cada audio se va a tener uno de estos
+    public void CambioDeVolumenGeneral()
+    {
+        audio.volume = slider.value;
+        PlayerPrefs.SetFloat("volumenGeneral", slider.value);
+    }
+}
